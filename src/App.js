0957832307be129da8d9aa28ab5bd0c42c5b2d9f
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-21 00:29:39 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Fot from "./component/footer";
+import Hed from "./component/header";
+function App() {
+  return (
+    <div className="App">
+      <Fot/>
+      <Hed/>
+    </div>
+  );
+}
+
+export default App;
